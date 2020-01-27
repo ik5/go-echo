7a3bf9b7344ffa3cc9bd975ab2a2ffa2d8a6efcd
Ido Kanner <idok@linesip.com>
2020-01-27 12:31:59 +0200

    print content as HTTP post

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 // DefaultPort is the default port to use if once is not specified by the SERVER_PORT environment variable
 const DefaultPort = "8080"
+
+// PeekReadCloser return a copy of a reader without loosing the original content
+func PeekReadCloser(stream *io.ReadCloser, cpy *[]byte) {
+	*cpy, _ = ioutil.ReadAll(*stream)
+	*stream = ioutil.NopCloser(bytes.NewReader(*cpy))
+}
 
 func getServerPort() string {
 	port := os.Getenv("SERVER_PORT")
@@ -27,20 +38,49 @@ func EchoHandler(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("Headers: %+v\n", request.Header)
 	if request.ContentLength > 0 {
 		defer request.Body.Close()
-		body, err := ioutil.ReadAll(request.Body)
-		log.Printf("Payload: %s | err: %s\n", body, err)
+		body := make([]byte, request.ContentLength+1)
+		PeekReadCloser(&request.Body, &body)
+		log.Printf("Payload: %s\n", body)
+
+		contentType := strings.Split(
+			strings.ToLower(request.Header.Get("content-type")),
+			";")
+		switch contentType[0] {
+		case "application/x-www-form-urlencoded":
+			err := request.ParseForm()
+			log.Printf("Form: %+v |err: %s\n", request.Form, err)
+			log.Printf("PostForm: %+v\n", request.PostForm)
+		case "multipart/form-data":
+			err := request.ParseMultipartForm(http.DefaultMaxHeaderBytes)
+			log.Printf("Form: %+v |err: %s\n", *request.MultipartForm, err)
+		}
 	} else {
 		log.Println("No Payload")
 	}
-	log.Println("---------------------------------------------------------------")
+	log.Println("---------------------------------------------------------------\n")
 
 	request.Write(writer)
 }
 
+func doServer() *http.Server {
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", EchoHandler)
+
+	var handler http.Handler = mux
+
+	srv := &http.Server{
+		ReadTimeout:  120 * time.Second,
+		WriteTimeout: 120 * time.Second,
+		IdleTimeout:  120 * time.Second, // introduced in Go 1.8
+		Handler:      handler,
+		Addr:         fmt.Sprintf(":%s", getServerPort()),
+	}
+
+	return srv
+}
+
 func main() {
+	log.Println("Starting server, listening on port " + getServerPort())
 
-	log.Println("starting server, listening on port " + getServerPort())
-
-	http.HandleFunc("/", EchoHandler)
-	http.ListenAndServe(":"+getServerPort(), nil)
+	doServer().ListenAndServe()
 }
